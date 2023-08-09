2b973ab8783b6705a5b496b221948d542887a568
Lilian Wanjiku <113428564+lilintech@users.noreply.github.com>
2023-08-09 19:45:30 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,43 @@
+//Create web server
+const express = require('express');
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+//Connect to database
+const Datastore = require('nedb');
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+//Get data from database
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    });
+});
+
+//Post data to database
+app.post('/api', (request, response) => {
+    console.log('I got a request!');
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json(data);
+});
+
+//Get data from database
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    });
+});
